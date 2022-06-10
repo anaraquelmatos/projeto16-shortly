25002762b0f3796b2592f8e5b93f5d31ff93a855
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-06-10 15:39:33 -0300

    fiz: database connection

--- database.js
+++ database.js
@@ -5,23 +5,11 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'filipeana';
-const host = 'localhost';
-const port = 5432;
-const database = 'postgres';
-
 const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
